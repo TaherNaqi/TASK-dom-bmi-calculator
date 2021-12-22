64ce07afe71920216d7286011afc67804e13b745
TaherNaqi <eng-naqi@hotmail.com>
2021-12-22 12:02:47 +0300

    BMI first commit

--- index.js
+++ index.js
@@ -8,4 +8,103 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+
+  //1. BMI calculator
+
+  height = height/100;
+  let BMI = (weight/(height*height));
+  alert(`Your BMI is ${BMI}`)
+
+  //Challenge 2
+
+  // if(BMI < 18.5){
+  //   alert(`You are underweight`)
+  // }
+  // else if(BMI < 24.9){
+  //   alert(`You are healthy Weight`)
+  // }
+  // else if(BMI < 30){
+  //   alert(`You are Overweight`)
+  // }
+  // else{
+  //   alert(`You are above obesity`)
+  
+  // Challenge 3:
+
+  if(age < 24){
+    if(BMI < 18.5){
+      alert(`You are underweight`)
+    }
+    else if(BMI < 24.9){
+      alert(`You are healthy Weight`)
+    }
+    else if(BMI < 30){
+      alert(`You are Overweight`)
+    }
+    else{
+      alert(`You are above obesity`)
+  }
 }
+  else if(age < 34){
+    if(BMI < 18.5){
+      alert(`You are underweight`)
+    }
+    else if(BMI < 24.9){
+      alert(`You are healthy Weight`)
+    }
+    else if(BMI < 30){
+      alert(`You are Overweight`)
+    }
+    else{
+      alert(`You are above obesity`)
+  }
+}
+
+  else if(age < 44){
+    if(BMI < 18.5){
+      alert(`You are underweight`)
+    }
+    else if(BMI < 24.9){
+      alert(`You are healthy Weight`)
+    }
+    else if(BMI < 30){
+      alert(`You are Overweight`)
+    }
+    else{
+      alert(`You are above obesity`)
+
+  }
+}
+  else if(age < 54){
+    if(BMI < 18.5){
+      alert(`You are underweight`)
+    }
+    else if(BMI < 24.9){
+      alert(`You are healthy Weight`)
+    }
+    else if(BMI < 30){
+      alert(`You are Overweight`)
+    }
+    else{
+      alert(`You are above obesity`)
+  }
+}
+
+  else if(age < 64){
+    if(BMI < 18.5){
+      alert(`You are underweight`)
+    }
+    else if(BMI < 24.9){
+      alert(`You are healthy Weight`)
+    }
+    else if(BMI < 30){
+      alert(`You are Overweight`)
+    }
+    else{
+      alert(`You are above obesity`)
+
+  }
+
+}
+}
+
